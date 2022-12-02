fa87bc7aec8dc527ef8e7f723c29748e2f3befae
gitdrik <58667795+gitdrik@users.noreply.github.com>
2022-12-02 15:52:57 +0100

    Add files via upload

--- 02.jl
+++ 02.jl
@@ -1,5 +1,5 @@
 open("02.txt") do f
     S = [(l[1]-'A', l[3]-'X') for l ∈ eachline(f)]
-    println("Part 1: ", sum([1+b+3*mod(1-a+b, 3) for (a,b) ∈ S]))
-    println("Part 2: ", sum([1+mod(2+a+b, 3)+3*b for (a,b) ∈ S]))
+    println("Part 1: ", sum([1 + b + 3*mod(1-a+b, 3) for (a,b) ∈ S]))
+    println("Part 2: ", sum([1 + mod(2+a+b, 3) + 3*b for (a,b) ∈ S]))
 end
