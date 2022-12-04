ea72f9df1dabf38049a8394e25cfe11456bccee4
gitdrik <58667795+gitdrik@users.noreply.github.com>
2022-12-04 17:12:52 +0100

    Add files via upload

--- 04.jl
+++ 04.jl
@@ -1,9 +1,9 @@
 open("04.txt") do f
-    W = [parse.(Int, split(l, r",|-")) for l in eachline(f)]
+    W = [parse.(Int, split(l, r",|-")) for l ∈ eachline(f)]
     p1, p2 = 0, 0
     for (a, b, c, d) ∈ W
         p1 += a:b ⊆ c:d || c:d ⊆ a:b
-        p2 += length((a:b) ∩ (c:d)) > 0
+        p2 += !isdisjoint(a:b, c:d)
     end
     println("Part 1: ", p1)
     println("Part 2: ", p2)
