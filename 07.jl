92822ed89138c367ae8897689def4dc8853203c8
gitdrik <58667795+gitdrik@users.noreply.github.com>
2022-12-07 08:55:32 +0100

    Add files via upload

---
+++ 07.jl
@@ -0,0 +1,30 @@
+using DataStructures
+
+open("07.txt") do f
+    path = []
+    dirsize = DefaultDict(0)
+    for l ∈ eachline(f)
+        if startswith(l, "\$ cd ..")
+            pop!(path)
+        elseif startswith(l, "\$ cd")
+            push!(path, split(l)[3])
+        else
+            if l[1] ∈ '0':'9'
+                size = parse(Int, split(l)[1])
+                for i ∈ eachindex(path)
+                    dirsize[path[1:i]] += size
+                end
+            end
+        end
+    end
+    println(sum([s for (_,s) ∈ dirsize if s ≤ 100000]))
+
+    S = dirsize[["/"]]
+    for s ∈ sort([s for (_,s) ∈ dirsize])
+        if 70000000-S+s ≥ 30000000
+            println(s)
+            break
+        end
+    end 
+
+end
