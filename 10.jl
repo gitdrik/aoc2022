77cbfcb075cef478b4839684198e3b26743c9245
gitdrik <58667795+gitdrik@users.noreply.github.com>
2022-12-10 15:35:51 +0100

    Add files via upload

---
+++ 10.jl
@@ -0,0 +1,20 @@
+function run()
+open("10.txt") do f
+    p1, i, X = 0, 0, 1
+    println("Part 2: ")
+    for l ∈ eachline(f)
+        print(i%40∈X-1:X+1 ? "█" : " ")
+        i%40==39 && println()
+        i += 1
+        (i-20) % 40 == 0 && (p1 += i*X)
+        if startswith(l, "addx")
+            print(i%40∈X-1:X+1 ? "█" : " ")
+            i%40==39 && println()
+            i += 1
+            (i-20) % 40 == 0 && (p1 += i*X)
+            X += parse(Int, split(l)[2])
+        end
+    end
+    println("Part 1: ", p1)
+end
+end
