301f3cb2770ecff97f176f4f411318b5efb5e8d7
gitdrik <58667795+gitdrik@users.noreply.github.com>
2022-12-12 11:27:33 +0100

    Add files via upload

---
+++ 12.jl
@@ -0,0 +1,51 @@
+open("12.txt") do f
+    M = Array{Char}(undef, 41, 113)
+    for (i, l) ∈ enumerate(eachline(f))
+        M[i,:] = collect(l)
+    end
+    @assert M[21,89]=='E'
+    M[21,89] = 'z'
+    goal = (21, 89)
+    @assert M[21,1]=='S'
+    M[21,1] = 'a'
+    start = (21,1)
+
+    function p1(start, goal)
+        Q = [(0, start...)]
+        seen = Set{Tuple{Int,Int}}()
+        while !isempty(Q)
+            steps, r, c = pop!(Q)
+            (r,c) ∈ seen && continue
+            push!(seen, (r, c))
+            (r,c) == goal && return steps
+            for (dr,dc) ∈ [(-1,0), (0,1), (1,0), (0,-1)]
+                nr, nc = r+dr, c+dc
+                (nr ∉ 1:41 || nc ∉ 1:113) && continue
+                M[nr,nc] - M[r,c] > 1 && continue
+                pushfirst!(Q, (steps+1,nr,nc))
+            end
+        end
+    end
+    println("Part 1: ", p1(start, goal))
+
+    function p2(goal)
+        ans = typemax(Int)
+        Q = [(0, goal...)]
+        seen = Set{Tuple{Int,Int}}()
+        while !isempty(Q)
+            steps, r, c = pop!(Q)
+            steps ≥ ans && continue
+            (r,c) ∈ seen && continue
+            push!(seen, (r, c))
+            M[r,c] == 'a' && (ans = min(ans, steps))
+            for (dr,dc) ∈ [(-1,0), (0,1), (1,0), (0,-1)]
+                nr, nc = r+dr, c+dc
+                (nr ∉ 1:41 || nc ∉ 1:113) && continue
+                M[nr,nc] - M[r,c] < -1 && continue
+                pushfirst!(Q, (steps+1,nr,nc))
+            end
+        end
+        return ans
+    end
+    println("Part 2: ", p2(goal))
+end
