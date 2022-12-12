5cc6f52ea0011b0ddd419fa75182e3ab9053b21a
gitdrik <58667795+gitdrik@users.noreply.github.com>
2022-12-12 18:35:07 +0100

    Add files via upload

--- 12.jl
+++ 12.jl
@@ -3,46 +3,29 @@ open("12.txt") do f
     for (i, l) ∈ enumerate(eachline(f))
         M[i,:] = collect(l)
     end
-    @assert M[21,89]=='E'
-    M[21,89] = 'z'
+    start = (21, 1)
+    @assert M[start...]=='S'
+    M[start...] = 'a'
     goal = (21, 89)
-    @assert M[21,1]=='S'
-    M[21,1] = 'a'
-    start = (21,1)
+    @assert M[goal...]=='E'
+    M[goal...] = 'z'
 
-    function p1(start, goal)
+    function solve(start, goal, slopes)
         Q = [(0, start...)]
         seen = Set{Tuple{Int,Int}}()
-        while !isempty(Q)
-            steps, r, c = pop!(Q)
+        for (steps, r, c) ∈ Q
             (r,c) ∈ seen && continue
             push!(seen, (r, c))
             (r,c) == goal && return steps
-            for (dr,dc) ∈ [(-1,0), (0,1), (1,0), (0,-1)]
+            M[r,c] == goal && return steps
+            for (dr, dc) ∈ [(-1,0), (0,1), (1,0), (0,-1)]
                 nr, nc = r+dr, c+dc
                 (nr ∉ 1:41 || nc ∉ 1:113) && continue
-                M[nr,nc] - M[r,c] > 1 && continue
-                pushfirst!(Q, (steps+1,nr,nc))
+                M[nr,nc] - M[r,c] ∉ slopes && continue
+                push!(Q, (steps+1, nr, nc))
             end
         end
     end
-    println("Part 1: ", p1(start, goal))
-
-    function p2(goal)
-        Q = [(0, goal...)]
-        seen = Set{Tuple{Int,Int}}()
-        while !isempty(Q)
-            steps, r, c = pop!(Q)
-            (r,c) ∈ seen && continue
-            push!(seen, (r, c))
-            M[r,c] == 'a' && return steps
-            for (dr,dc) ∈ [(-1,0), (0,1), (1,0), (0,-1)]
-                nr, nc = r+dr, c+dc
-                (nr ∉ 1:41 || nc ∉ 1:113) && continue
-                M[nr,nc] - M[r,c] < -1 && continue
-                pushfirst!(Q, (steps+1,nr,nc))
-            end
-        end
-    end
-    println("Part 2: ", p2(goal))
+    println("Part 1: ", solve(start, goal, typemin(Int):1))
+    println("Part 2: ", solve(goal, 'a', -1:typemax(Int)))
 end
