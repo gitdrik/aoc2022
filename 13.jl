d4bdcd0024cca4ffc43177efea2ee6ab496250c0
gitdrik <58667795+gitdrik@users.noreply.github.com>
2022-12-13 19:39:17 +0100

    Add files via upload

--- 13.jl
+++ 13.jl
@@ -1,4 +1,13 @@
 open("13.txt") do f
+    P1, P2 = [], []
+    while !eof(f)
+        L = eval(Meta.parse(readline(f)))
+        R = eval(Meta.parse(readline(f)))
+        push!(P1, [L, R])
+        push!(P2, L, R)
+        readline(f)
+    end
+
     function compare(L, R)
         L==R && return nothing
         L == [] && return true
@@ -12,32 +21,9 @@ open("13.txt") do f
         return compare(L, R)
     end
 
-    function insort!(P, p)
-        done = false
-        for i ∈ eachindex(P)
-            if compare(p, P[i])
-                splice!(P, i:i-1, [p])
-                done = true
-                break
-            end
-        end
-        !done && (push!(P, p))
-        return P
-    end
-
-    P1, P2 = [], []
-    while !eof(f)
-        L = eval(Meta.parse(readline(f)))
-        R = eval(Meta.parse(readline(f)))
-        push!(P1, [L, R])
-        insort!(P2, L)
-        insort!(P2, R)
-        readline(f)
-    end
-
     println("Part 1: ", sum(findall([compare(L,R) for (L,R) ∈ P1])))
 
-    insort!(P2, [[2]])
-    insort!(P2, [[6]])
+    push!(P2, [[2]], [[6]])
+    sort!(P2, lt = compare)
     println("Part 2: ", findfirst(==([[2]]), P2) * findfirst(==([[6]]), P2))
 end
