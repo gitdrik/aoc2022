8e41a9f4002a4237c39dcce30b6776ec12ce91a9
gitdrik <58667795+gitdrik@users.noreply.github.com>
2022-12-20 14:56:11 +0100

    Add files via upload

--- 19.jl
+++ 19.jl
@@ -9,13 +9,28 @@ open("19.txt") do f
                   [l[28],     0, l[31], 0]])
     end
 
+    function geobound(r, s, b, t)
+        # Make a maximum estimate based on geodes and obisdian
+        # (unlimited clay and ore)
+        obs = s[3]
+        obsr = r[3]
+        geo = s[4]
+        geor = r[4]
+        for tt ∈ t:-1:1          
+            pass
+        end
+    end
+
     function make(b, robots, stuff, time)
         Q = Set([(robots, stuff, time)])
+        #Global best solution
         maxgeode = 0
+        #Maximum number of ore, clay and obsidian robots that make sense
+        maxore   = max(b[2][1],b[3][1],b[4][1])
+#        maxclay  = max(b[3][2],b[4][2])
+#        maxobs   = max(b[4][3])
         while !isempty(Q)
             (r, s, t) = pop!(Q)
-#            s[4]+r[4]*t+(t*t+t)÷2 ≤ maxgeode && continue
-            s[4] + (r[4]+2)*t ≤ maxgeode && continue
             if t==0
                 if s[4] > maxgeode
                     maxgeode = s[4]
@@ -23,9 +38,15 @@ open("19.txt") do f
                 end
                 continue
             end
+            #Estimate if it is possible to beat the best previously found soutin
+            tt = t-1
+            s[4] + t*r[4] + (tt*tt+tt)÷2 ≤ maxgeode && continue
+            #s[4] + (r[4]+2)*t ≤ maxgeode && continue
+
             push!(Q, (r, s.+r, t-1))
 
             for i ∈ 1:4
+                i==1 && r[1]==maxore && continue
                 if all(s .≥ b[i])
                     rr = copy(r)
                     ss = copy(s)
